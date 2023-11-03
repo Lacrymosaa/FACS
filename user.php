909d26805c82d7caa766bb596f77539a5f47a0d7
SamuelThales <95038076+Samoo2@users.noreply.github.com>
2023-11-03 00:01:54 -0300

    Update user.php

--- user.php
+++ user.php
@@ -33,34 +33,5 @@ class User
             die('Falha ao registrar o usuário: ' . $e->getMessage());
         }
     }
-    public function logar($email, $senha)
-{
-    try {
-        // Verificar se o email existe na tabela
-        $sql = "SELECT * FROM usuario WHERE email = ?";
-        $user = $this->connection->Consult($sql, array($email));
-
-        if (count($user) > 0) {
-            // Verificar se a senha corresponde à senha armazenada no banco de dados
-            $senha_hash = $user[0]['senha_hash'];
-            if (password_verify($senha, $senha_hash)) {
-                // A senha está correta, o login é bem-sucedido
-                return true;
-            } else {
-                // Senha incorreta
-                echo "Senha incorreta.";
-                return false;
-            }
-        } else {
-            // Email não encontrado na tabela
-            echo "Email não cadastrado.";
-            return false;
-        }
-    } catch (PDOException $e) {
-        die('Falha ao fazer login: ' . $e->getMessage());
-    }
-}
-
-
 }
 ?>
