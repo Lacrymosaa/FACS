06a7fe2e2c2adfd89a7ed61673e28f3aef0e3f4b
marian <marianvulpes1987@gmail.com>
2023-11-12 10:15:18 -0300

    Adicionar Classe Patrimônio e Mudanças no Banco
    Adicionada a classe patrimônio, já com as funções de cadastro, edição, listagem por nota e departamento.
    
    Mudanças no banco de dados para unificar todas as tabelas de itens em uma única, adicionando uma nova linha que indicaria qual é o objeto (nome_objeto), que será utilizado para listagem também.

---
+++ CLASSpatrimonio.php
@@ -0,0 +1,105 @@
+<?php
+
+require('./assets/config/connect.php');
+
+class Patrimonio
+{
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function cadastrarPatrimonio($numero_patrimonio, $id_departamento, $conservacao, $observacao, $tipo, $condicao, $path_foto, $id_nota_fiscal, $nome_objeto)
+    {
+        try {
+            $stmt = $this->conn->prepare("INSERT INTO Patrimonio (numero_patrimonio, id_departamento, conservacao, observacao, tipo, condicao, path_foto, id_nota_fiscal, nome_objeto) 
+                                          VALUES (:numero_patrimonio, :id_departamento, :conservacao, :observacao, :tipo, :condicao, :path_foto, :id_nota_fiscal, :nome_objeto)");
+
+            $stmt->bindParam(':numero_patrimonio', $numero_patrimonio);
+            $stmt->bindParam(':id_departamento', $id_departamento);
+            $stmt->bindParam(':conservacao', $conservacao);
+            $stmt->bindParam(':observacao', $observacao);
+            $stmt->bindParam(':tipo', $tipo);
+            $stmt->bindParam(':condicao', $condicao);
+            $stmt->bindParam(':path_foto', $path_foto);
+            $stmt->bindParam(':id_nota_fiscal', $id_nota_fiscal);
+            $stmt->bindParam(':nome_objeto', $nome_objeto);
+
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function editarPatrimonio($id_patrimonio, $numero_patrimonio, $id_departamento, $conservacao, $observacao, $tipo, $condicao, $path_foto, $id_nota_fiscal, $nome_objeto)
+    {
+        try {
+            $stmt = $this->conn->prepare("UPDATE Patrimonio 
+                                          SET numero_patrimonio = :numero_patrimonio, 
+                                              id_departamento = :id_departamento, 
+                                              conservacao = :conservacao, 
+                                              observacao = :observacao, 
+                                              tipo = :tipo, 
+                                              condicao = :condicao, 
+                                              path_foto = :path_foto, 
+                                              id_nota_fiscal = :id_nota_fiscal, 
+                                              nome_objeto = :nome_objeto 
+                                          WHERE id_patrimonio = :id_patrimonio");
+
+            $stmt->bindParam(':id_patrimonio', $id_patrimonio);
+            $stmt->bindParam(':numero_patrimonio', $numero_patrimonio);
+            $stmt->bindParam(':id_departamento', $id_departamento);
+            $stmt->bindParam(':conservacao', $conservacao);
+            $stmt->bindParam(':observacao', $observacao);
+            $stmt->bindParam(':tipo', $tipo);
+            $stmt->bindParam(':condicao', $condicao);
+            $stmt->bindParam(':path_foto', $path_foto);
+            $stmt->bindParam(':id_nota_fiscal', $id_nota_fiscal);
+            $stmt->bindParam(':nome_objeto', $nome_objeto);
+
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function listarPatrimonioPorNota($id_nota_fiscal)
+    {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM Patrimonio WHERE id_nota_fiscal = :id_nota_fiscal");
+            $stmt->bindParam(':id_nota_fiscal', $id_nota_fiscal);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function listarPatrimonioPorDepartamento($id_departamento)
+    {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM Patrimonio WHERE id_departamento = :id_departamento");
+            $stmt->bindParam(':id_departamento', $id_departamento);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+}
+
+?>
--- modelagem.sql
+++ modelagem.sql
@@ -30,137 +30,18 @@ CREATE TABLE NotaFiscal (
     nf_file VARCHAR(100) NOT NULL
 );
 
--- Tabela Armario
-CREATE TABLE Armario (
-    id_armario INT PRIMARY KEY AUTO_INCREMENT,
-    armario_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    armario_conservacao VARCHAR(50) NOT NULL,
-    armario_observacao VARCHAR(200) NOT NULL,
-    armario_tipo VARCHAR(50) NOT NULL,
-    armario_status BIT NOT NULL,
-    armario_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
 
--- Tabela Cadeira
-CREATE TABLE Cadeira (
-    id_cadeira INT PRIMARY KEY AUTO_INCREMENT,
-    cadeira_numero_patrimonio INT NOT NULL,
+CREATE TABLE Patrimonio (
+    id_patrimonio INT PRIMARY KEY AUTO_INCREMENT,
+    numero_patrimonio INT NOT NULL,
+    conservacao VARCHAR(50) NOT NULL,
+    observacao VARCHAR(200) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    condicao BIT NOT NULL,
+    path_foto VARCHAR(100) NOT NULL,
+    nome_objeto VARCHAR(50) NOT NULL,
+    id_nota_fiscal INT NOT NULL, 
     id_departamento INT NOT NULL,
-    cadeira_conservacao VARCHAR(50) NOT NULL,
-    cadeira_observacao VARCHAR(200) NOT NULL,
-    cadeira_tipo VARCHAR(50) NOT NULL,
-    cadeira_status BIT NOT NULL,
-    cadeira_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Informatica
-CREATE TABLE Informatica (
-    id_informatica INT PRIMARY KEY AUTO_INCREMENT,
-    informatica_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    informatica_conservacao VARCHAR(50) NOT NULL,
-    informatica_observacao VARCHAR(200) NOT NULL,
-    informatica_tipo VARCHAR(50) NOT NULL,
-    informatica_status BIT NOT NULL,
-    informatica_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Eletrodomestico
-CREATE TABLE Eletrodomestico (
-    id_eletrodomestico INT PRIMARY KEY AUTO_INCREMENT,
-    eletrodomestico_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    eletrodomestico_conservacao VARCHAR(50) NOT NULL,
-    eletrodomestico_observacao VARCHAR(200) NOT NULL,
-    eletrodomestico_tipo VARCHAR(50) NOT NULL,
-    eletrodomestico_status BIT NOT NULL,
-    eletrodomestico_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Mesa
-CREATE TABLE Mesa (
-    id_mesa INT PRIMARY KEY AUTO_INCREMENT,
-    mesa_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    mesa_conservacao VARCHAR(50) NOT NULL,
-    mesa_observacao VARCHAR(200) NOT NULL, 
-    mesa_tipo VARCHAR(50) NOT NULL,
-    mesa_status BIT NOT NULL,
-    mesa_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Patrimonio Diverso
-CREATE TABLE PatrimonioDiverso (
-    id_patrimonio_diverso INT PRIMARY KEY AUTO_INCREMENT,
-    patrimonio_diverso_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    patrimonio_diverso_conservacao VARCHAR(50) NOT NULL,
-    patrimonio_diverso_observacao VARCHAR(200) NOT NULL,
-    patrimonio_diverso_tipo VARCHAR(50) NOT NULL,
-    patrimonio_diverso_status BIT NOT NULL,
-    patrimonio_diverso_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Poltrona
-CREATE TABLE Poltrona (
-    id_poltrona INT PRIMARY KEY AUTO_INCREMENT,
-    poltrona_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    poltrona_conservacao VARCHAR(50) NOT NULL,
-    poltrona_observacao VARCHAR(200) NOT NULL,
-    poltrona_tipo VARCHAR(50) NOT NULL,
-    poltrona_status BIT NOT NULL,
-    poltrona_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Som e Imagem
-CREATE TABLE SomImagem (
-    id_som_imagem INT PRIMARY KEY AUTO_INCREMENT,
-    som_imagem_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    som_imagem_conservacao VARCHAR(50) NOT NULL, 
-    som_imagem_observacao VARCHAR(200) NOT NULL,
-    som_imagem_tipo VARCHAR(50) NOT NULL,
-    som_imagem_status BIT NOT NULL,
-    som_imagem_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
-
--- Tabela Telefone
-CREATE TABLE Telefone (
-    id_telefone INT PRIMARY KEY AUTO_INCREMENT,
-    telefone_numero_patrimonio INT NOT NULL,
-    id_departamento INT NOT NULL,
-    telefone_conservacao VARCHAR(50) NOT NULL,
-    telefone_observacao VARCHAR(200) NOT NULL,
-    telefone_tipo VARCHAR(50) NOT NULL,
-    telefone_status BIT NOT NULL,
-    telefone_path_foto VARCHAR(100) NOT NULL,
-    id_nota_fiscal INT NOT NULL,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
-    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal)
-);
+    FOREIGN KEY (id_nota_fiscal) REFERENCES NotaFiscal(id_nota_fiscal),
+    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)
+); 
